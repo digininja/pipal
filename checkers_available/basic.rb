0bdc0105e4dad8afe6b101081d26626f7c481228
Robin Wood <robin@digininja.org>
2014-06-30 13:56:14 +0100

    one to six percentage was always 0 because of variable typo

--- checkers_available/basic.rb
+++ checkers_available/basic.rb
@@ -235,7 +235,7 @@ class Basic_Checker < Checker
 		horiz.generate
 		ret_str << horiz.graph
 
-		ret_str << "\nOne to six characters = #{@one_to_six_chars.to_s} (#{((@ne_to_six_chars.to_f/@total_words_processed) * 100).round(2).to_s}%)\n"
+		ret_str << "\nOne to six characters = #{@one_to_six_chars.to_s} (#{((@one_to_six_chars.to_f/@total_words_processed) * 100).round(2).to_s}%)\n"
 		ret_str << "One to eight characters = #{@one_to_eight_chars.to_s} (#{((@one_to_eight_chars.to_f/@total_words_processed) * 100).round(2).to_s}'%)\n"
 		ret_str << "More than eight characters = #{@over_eight_chars.to_s} (#{((@over_eight_chars.to_f/@total_words_processed) * 100).round(2).to_s}%)\n"
 
