83ca1d76cb057a3e29f796c5129bf929cdc1a79a
robin <robin@digi.ninja>
2015-12-22 22:31:28 +0000

    changed variable name

--- list_checker.rb
+++ list_checker.rb
@@ -6,9 +6,9 @@ class List_Checker < Checker
 	end
 
 	def process_word (word, extras = nil)
-		@list.each_pair do |colour, count|
-			if /#{colour}/i.match word
-				@list[colour] += 1
+		@list.each_pair do |word, count|
+			if /#{word}/i.match word
+				@list[word] += 1
 			end
 		end
 		@total_words_processed += 1
@@ -18,10 +18,10 @@ class List_Checker < Checker
 		ret_str = "#{title}\n"
 		disp = false
 
-		(@list.sort do |x,y| (x[1] <=> y[1]) * -1 end).each do |colour_data|
-			unless colour_data[1] == 0
+		(@list.sort do |x,y| (x[1] <=> y[1]) * -1 end).each do |word_data|
+			unless word_data[1] == 0
 				disp = true
-				ret_str << "#{colour_data[0]} = #{colour_data[1].to_s} (#{((colour_data[1].to_f/@total_words_processed) * 100).round(2).to_s}%)\n"
+				ret_str << "#{word_data[0]} = #{word_data[1].to_s} (#{((word_data[1].to_f/@total_words_processed) * 100).round(2).to_s}%)\n"
 			end
 		end
 		unless disp
