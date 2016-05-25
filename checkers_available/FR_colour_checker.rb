487872e6ebb0eacc854244cc3f2d897477a30857
Matthieu Keller <keller.mdpa@gmail.com>
2016-05-25 15:16:01 +0200

    Correct French typo

--- checkers_available/FR_colour_checker.rb
+++ checkers_available/FR_colour_checker.rb
@@ -6,7 +6,7 @@ class FR_Colour_Checker < Checker
 	def initialize
 		super
 
-		@list = {"noir" => 0, "bleu" => 0, "maron" => 0, "gris" => 0, "vert" => 0,
+		@list = {"noir" => 0, "bleu" => 0, "marron" => 0, "gris" => 0, "vert" => 0,
 		"orange" => 0, "rose" => 0, "rouge" => 0, "blanc" => 0, "jaune" => 0, 
 		"violet" => 0, "indigo" => 0, "ocre" => 0, "cyan" => 0, "ocre" => 0}
 		@description = "List of common French colours"
