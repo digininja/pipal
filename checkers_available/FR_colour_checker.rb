3f04db2d151d46417a6406569fcf28f98c3bd7f0
Matthieu Keller <keller.mdpa@gmail.com>
2016-05-25 15:16:23 +0200

    Add brown color

--- checkers_available/FR_colour_checker.rb
+++ checkers_available/FR_colour_checker.rb
@@ -8,7 +8,8 @@ class FR_Colour_Checker < Checker
 
 		@list = {"noir" => 0, "bleu" => 0, "marron" => 0, "gris" => 0, "vert" => 0,
 		"orange" => 0, "rose" => 0, "rouge" => 0, "blanc" => 0, "jaune" => 0, 
-		"violet" => 0, "indigo" => 0, "ocre" => 0, "cyan" => 0, "ocre" => 0}
+		"violet" => 0, "indigo" => 0, "ocre" => 0, "cyan" => 0, "ocre" => 0,
+		"brun" =>0 }
 		@description = "List of common French colours"
 	end
 
